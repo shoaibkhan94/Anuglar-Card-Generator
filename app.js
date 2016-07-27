de2e9c74bca08749207f68ba2108b41cc9dfe6b7
shoaib Khan <shoaibk@shoaibk01.connvertex.local>
2016-07-27 14:50:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by shoaibk on 27/7/16.
+ */
+(function(){
+    var app=angular.module('myApp',[]);
+    app.controller('BusinessCardController', function(){
+        this.user={
+            name:'Shoaib Khan'
+        };
+    });
+
+
+})();
